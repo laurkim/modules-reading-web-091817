1e262db889fd14689731f8f05e3fdf7a9273efdf
laurkim <laurakeem@gmail.com>
2017-09-27 23:44:38 -0400

    Done.

--- lib/dancer.rb
+++ lib/dancer.rb
@@ -1,2 +1,16 @@
 require_relative './class_methods_module.rb'
 require_relative './dance_module.rb'
+require_relative './fancy_dance.rb'
+
+class Dancer
+
+  include FancyDance::InstanceMethods #calls instance methods on our instance (i.e., Katie)
+  extend FancyDance::ClassMethods #calls class methods on our entire class (i.e., output message)
+
+  attr_accessor :name
+
+  def initialize(name)
+    @name = name
+  end
+
+end
